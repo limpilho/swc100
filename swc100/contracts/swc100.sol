7487ffe1bd82f0d6a297bc59b40b345f5f2faa25
piro <43782319+limpilho@users.noreply.github.com>
2021-07-31 00:11:09 +0900

    Update swc100.sol

--- swc100/contracts/swc100.sol
+++ swc100/contracts/swc100.sol
@@ -2,8 +2,6 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HashForEther {
-    //x = account[9]
-    //address x = 0x14736bB05D262E93873ee42eE4AEe3323c3b2c46;
     function withdrawWinnings() public {
         // Winner if the last 8 hex characters of the address are 0. 
         require(uint32(msg.sender) == 0,
